47cd6ef40aa8e7f971629f7d97e8eb9c4d3fafe8
Monica Hong <momo.star@gmail.com>
2021-02-21 09:14:07 -0800

    update typing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 $(document).ready(function () {
-
+  console.log("document is ready!");
 });
+/*
+https://github.com/daybrush/scenejs
+https://github.com/daybrush/scenejs-effects
+*/
+new Scene(
+  {
+    ".container1 span": Scene.typing({
+      text: "Full-Stack web Developer At your service!",
+      duration: 7,
+      delay: 0.5,
+    }),
+    ".cursor": {
+      0: { opacity: 0 },
+      0.5: { opacity: 0 },
+      "0.5>": { opacity: 1 },
+      1: { opacity: 1 },
+      options: {
+        iterationCount: 8,
+      },
+    },
+  },
+  {
+    iterationCount: "infinite",
+    direction: "alternate",
+    selector: true,
+  }
+).play();
