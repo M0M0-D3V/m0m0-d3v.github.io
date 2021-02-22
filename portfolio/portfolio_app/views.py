4d6c652073a0b7929d9440ab8cac99cd4e3f5c4e
Monica Hong <momo.star@gmail.com>
2021-02-22 09:01:13 -0800

    index change loop

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Project, Image, Tech, TeamMember, Message
 # Create your views here.
 
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "all_projects": Project.objects.all()
+    }
+    return render(request, "index.html", context)
